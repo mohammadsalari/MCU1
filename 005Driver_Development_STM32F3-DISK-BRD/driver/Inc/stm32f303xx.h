c1351d3636bf37a6b7765a9863064fbb9d051d2b
mohammadsalari98351 <mohammadsalari98351@gmail.com>
2022-08-03 10:41:02 +0430

    defining base addresses of different memories

--- 005Driver_Development_STM32F3-DISK-BRD/driver/Inc/stm32f303xx.h
+++ 005Driver_Development_STM32F3-DISK-BRD/driver/Inc/stm32f303xx.h
@@ -8,6 +8,12 @@
 #ifndef INC_STM32F303XX_H_
 #define INC_STM32F303XX_H_
 
+/*
+ * base addresses of SRAM and FLASH memories
+ */
+#define FLASH_BASEADDR						0x08000000UL	//256KB
+#define SRAM_BASEADDR						0x20000000UL	//40KB
+#define ROM_BASEADDR /*=system memory*/		0x1FFFD800UL	//8KB
 
 
 #endif /* INC_STM32F303XX_H_ */
