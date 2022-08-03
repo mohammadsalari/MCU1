0be4fb2219c6cbc08f8b92aef130e4c010955337
mohammadsalari98351 <mohammadsalari98351@gmail.com>
2022-08-03 10:55:01 +0430

    defining base addresses of different bus domains.

--- 005Driver_Development_STM32F3-DISK-BRD/driver/Inc/stm32f303xx.h
+++ 005Driver_Development_STM32F3-DISK-BRD/driver/Inc/stm32f303xx.h
@@ -16,4 +16,14 @@
 #define ROM_BASEADDR /*=system memory*/		0x1FFFD800UL	//8KB
 
 
+/*
+ * base addresses of different peripheral buses
+ */
+#define APB1_BASEADDR						0x40000000U
+#define APB2_BASEADDR						0x40010000U
+#define AHB1_BASEADDR						0x40020000U
+
+
+
+
 #endif /* INC_STM32F303XX_H_ */
