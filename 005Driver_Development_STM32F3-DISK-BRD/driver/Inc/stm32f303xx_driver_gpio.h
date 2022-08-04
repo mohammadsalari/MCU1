dc00b64706c78d9a4eb53e89d2456d0dcd171604
mohammadsalari98351 <mohammadsalari98351@gmail.com>
2022-08-05 01:31:32 +0430

    creating both the source file and the header file for the GPIO driver

---
+++ 005Driver_Development_STM32F3-DISK-BRD/driver/Inc/stm32f303xx_driver_gpio.h
@@ -0,0 +1,14 @@
+/*
+ * stm32f303xx_driver_gpio.h
+ *
+ *  Created on: Aug 5, 2022
+ *      Author: salari
+ */
+
+#ifndef INC_STM32F303XX_DRIVER_GPIO_H_
+#define INC_STM32F303XX_DRIVER_GPIO_H_
+
+#include "stm32f303xx.h"
+
+
+#endif /* INC_STM32F303XX_DRIVER_GPIO_H_ */
---
+++ 005Driver_Development_STM32F3-DISK-BRD/driver/Src/stm32f303xx_driver_gpio.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f303xx_driver_gpio.c
+ *
+ *  Created on: Aug 5, 2022
+ *      Author: salari
+ */
+#include "stm32f303xx_driver_gpio.h"
+
